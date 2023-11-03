3ee35ceaa6374abb93babc5753e7832a93d3bfbf
lyuqinshu <lyuqinshu@outlook.com>
2023-11-03 15:21:27 +0000

    Update

--- pycaf/__init__.py
+++ pycaf/__init__.py
@@ -2,3 +2,4 @@ from .experiment import Experiment
 from .mot_auto_tuner import MOTAutoTuner
 from .pattern_visualizer import PatternVisualizer
 from .live_mot import LiveMOT
+from .mot_auto_tuner_wml import MOTAutoTunerWML
